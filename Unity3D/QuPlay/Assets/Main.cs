d7b2e23a4a84105a2102062abfa69e574f893546
Sky Rolnick <skymeson@gmail.com>
2019-12-30 05:26:16 -0500

    Simtools Test Scene

--- Unity3D/QuPlay/Assets/Main.cs
+++ Unity3D/QuPlay/Assets/Main.cs
@@ -5,6 +5,15 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        QuPlaySimtools.QuSimtools_SendTelemetry(Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180), Random.RandomRange(-180, 180));
+        QuPlaySimtools.QuSimtools_SendTelemetry(
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180), 
+            Random.RandomRange(-180, 180));
     }
 }
---
+++ Unity3D/QuPlay/Assets/MainSlider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class MainSlider : MonoBehaviour
+{
+
+    public Slider sliderRoll;
+    public Slider sliderPitch;
+    public Slider sliderYaw;
+    public Slider sliderHeave;
+    public Slider sliderSway;
+    public Slider sliderSurge;
+    public Slider sliderExtra1;
+    public Slider sliderExtra2;
+    public Slider sliderExtra3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        sliderRoll.onValueChanged.AddListener(delegate { ValueChangeCheck(sliderRoll); });
+        sliderPitch.onValueChanged.AddListener(delegate { ValueChangeCheck(sliderPitch); });
+        sliderYaw.onValueChanged.AddListener(delegate { ValueChangeCheck(sliderYaw); });
+
+
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck(Slider slider)
+    {
+        Debug.Log("OnValueChanged: " + slider.gameObject.name + " " +slider.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //QuPlaySimtools.QuSimtools_SendTelemetry(
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180),
+        //    Random.RandomRange(-180, 180));
+
+        Debug.Log(string.Format("Sending Telemetry data : {0} : {1} : {2}", sliderRoll.value, sliderPitch.value, sliderYaw.value));
+        QuPlaySimtools.QuSimtools_SendTelemetry(
+                    sliderRoll.value,
+                    sliderPitch.value,
+                    sliderYaw.value,
+                    0f,
+                    0f,
+                    0f,
+                    Random.RandomRange(-180, 180),
+                    Random.RandomRange(-180, 180),
+                    Random.RandomRange(-180, 180));
+    }
+}
